83b3f675eb1fa9611388528cdba236e2fe965438
Clark Hale <chale@redhat.com>
2020-06-10 14:49:31 -0400

    Added env check.

--- src/main/java/net/deadvax/examples/App.java
+++ src/main/java/net/deadvax/examples/App.java
@@ -1,13 +1,23 @@
 package net.deadvax.examples;
 
+import java.lang.Thread;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws java.lang.InterruptedException
     {
         System.out.println( "Hello World!" );
+
+	int i = 5;
+
+	while( i > 0 ) {
+		System.out.println("Environment Definition:  " + System.getProperty("JOB_TEST"));
+		Thread.sleep(1);
+
+	}
     }
 }
